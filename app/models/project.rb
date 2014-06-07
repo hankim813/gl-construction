0bd0e0dab9cb704d456b906244fa5f43683158a6
Han Kim <hankim813@gmail.com>
2014-06-07 17:53:02 -0400

    Added validations and User Name

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,8 @@
 class Project < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image
+
+	validates :image, presence: true
+	validates :description, presence: true
+	validates :title, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140607212939) do
+ActiveRecord::Schema.define(version: 20140607214852) do
 
   create_table "projects", force: true do |t|
     t.text     "description"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20140607212939) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
