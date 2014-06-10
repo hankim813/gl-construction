ecbe7530bdc7bf65439940cf275c2ffd9baeec72
Han Kim <hankim813@gmail.com>
2014-06-10 13:24:22 -0400

    projects page v2

---
+++ app/assets/javascripts/pj.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$("#completed-proj").click(function() {
+		$("#the-projects").fadeOut("slow")
+	});
+
+	$("#current-proj").click(function() {
+		$("#the-projects").fadeIn("slow")
+	})
+});
+
